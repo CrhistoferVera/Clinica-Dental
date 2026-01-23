119e59c818afe213ff9c95ae3876dcf10f8aaf33
CrhistoferVera <cristofervera110@gmail.com>
2026-01-23 19:59:34 -0400

    Edit some models

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,6 +33,12 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        $user = Auth::user();
+
+        if ($user->hasRole('paciente')) {
+            return redirect()->intended('/portal-paciente');
+        }
+
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
